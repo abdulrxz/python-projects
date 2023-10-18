8324c75ab101e54ef41240ad793478b2c02db03c
Abdul Rehman <137254419+abdulrxz@users.noreply.github.com>
2023-10-18 15:11:34 +0500

    Create word_replacement.py

---
+++ word_replacement.py
@@ -0,0 +1,7 @@
+def replace_word():
+    str="Hi guy, i am abdul, and i am a data scientist. "
+    word_to_replce=input("Enter the word to replace:")
+    word_replacement=input("Enter the word replacement:")
+    print(str.replace(word_to_replce,word_replacement))
+
+replace_word()   
